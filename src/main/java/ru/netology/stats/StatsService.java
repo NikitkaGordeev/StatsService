ca309bb54c1e2a43514c26b7932985c3147e7449
Nikita G <jobnikitka152rus@gmail.com>
2023-05-20 08:56:47 +0300

    reformat code

--- src/main/java/ru/netology/stats/StatsService.java
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -2,27 +2,27 @@ package ru.netology.stats;
 
 public class StatsService {
     public int minSales(long[] sales) {
-        int minMonth = 0; // номер месяца с минимальными продажами среди просмотренных ранее
+        int minMonth = 0;
 
         for (int i = 0; i < sales.length; i++) {
-            if (sales[i] <= sales[minMonth]) { // значит, в рассматриваемом i-м месяце продаж меньше
-                minMonth = i; // запомним его как минимальный
+            if (sales[i] <= sales[minMonth]) {
+                minMonth = i;
             }
         }
 
-        return minMonth + 1; // месяца нумеруются с 1, а индексы массива с 0, нужно сдвинуть ответ на 1
+        return minMonth + 1;
     }
 
     public int maxSales(long[] sales) {
-        int maxMonth = 0; // номер месяца с максимальными продажами среди просмотренных ранее
+        int maxMonth = 0;
 
         for (int i = 0; i < sales.length; i++) {
-            if (sales[i] >= sales[maxMonth]) { // значит, в рассматриваемом i-м месяце продаж меньше
-                maxMonth = i; // запомним его как минимальный
+            if (sales[i] >= sales[maxMonth]) {
+                maxMonth = i;
             }
         }
 
-        return maxMonth + 1; // месяца нумеруются с 1, а индексы массива с 0, нужно сдвинуть ответ на 1
+        return maxMonth + 1;
     }
 
     public int sumSales(long[] sales) {
